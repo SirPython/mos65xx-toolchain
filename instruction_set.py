70fb0602a3abdab1fdb9500c89d6da1330f2b2c1
Anthony <anoky96@gmail.com>
2020-09-03 18:01:38 -0400

    finished blocks 2 and 3

--- instruction_set.py
+++ instruction_set.py
@@ -55,8 +55,16 @@ class AbsoluteYInsturction(Instruction):
     def __call__(self, data, mos):
         self.fn(mos[data[0] + data[1] + mos.index_y], mos)
 class RelativeInstruction(Instruction):
+    def __init__(self, status_flag, cond_val, num_cycles, num_bytes):
+        self.status_flag = status_flag
+        self.cond_val = cond_val
+
+        self.num_cycles = num_cycles
+        self.num_bytes = num_bytes
+
     def __call__(self, data, mos):
-        self.fn(data[0], mos)
+        if mos[self.status_flag] == self.cond_val:
+            mos["pc"] = (mos["pc"] & 0xFF) + data
 class IndirectInstruction(Instruction):
     def __call__(self, data, mos):
         addr = data[0] + (data[1] << 8)
@@ -66,7 +74,7 @@ class ZeroPageYInstruction(Instruction):
         self.fn(mos[data[0] + mos.index_y], mos)
 
 def ADC(data, mos):
-    mos.accumulator += data
+    mos["a"] += data
 ADC_IMMEDIATE = ImmediateInstruction(ADC, 2, 2)
 ADC_ABSOLUTE  = AbsoluteInstruction (ADC, 4, 3)
 ADC_ZEROPAGE  = ZeroPageInstruction (ADC, 3, 2)
@@ -77,7 +85,7 @@ ADC_ABSOLUTEX = AbsoluteXInstruction(ADC, 4, 3)
 ADC_ABSOLUTEY = AbsoluteYInsturction(ADC, 4, 3)
 
 def AND(data, mos):
-    mos.accumulator = mos.accumulator & data
+    mos["a"] = mos["a"] & data
 AND_IMMEDIATE = ImmediateInstruction(AND, 2, 2)
 AND_ABSOLUTE  = AbsoluteInstruction (AND, 4, 3)
 AND_ZEROPAGE  = ZeroPageInstruction (AND, 3, 2)
@@ -96,12 +104,40 @@ ASL_ACCUMULATOR = AccumulatorInstruction(ASL, 2, 1)
 ASL_ZEROPAGEX   = ZeroPageXInstruction  (6, 2)
 ASL_ABSOLUTEX   = AbsoluteXInstruction  (7, 3)
 
-def BCC(data, mos):
-    if mos["carry"] == 0:
-        mos["pc"] = (mos["pc"] & 0xFF) + data
-BCC_RELATIVE = RelativeInstruction(BCC, 2, 2)
+BCC_RELATIVE = RelativeInstruction("carry", 0, 2, 2)
+BCS_RELATIVE = RelativeInstruction("carry", 1, 2, 2)
+BEQ_RELATIVE = RelativeInstruction("zero",  1, 2, 2)
 
-def BCS(data, mos):
-    if mos["carry"] == 1:
-        mos["pc"] = (mos["pc"] & 0xFF) + data
-BCS_RELATIVE = RelativeInstruction(BCS, 2, 2)
+def BIT(data, mos):
+    res = (mos["a"] & data) << 6
+    mos["overflow"] = res & 0x01
+    mos["negative"] = res << 1
+BIT_ABSOLUTE = AbsoluteInstruction(BIT, 4, 3)
+BIT_ZEROPAGE = ZeroPageInstruction(BIT, 3, 2)
+
+BMI_RELATIVE = RelativeInstruction("negative", 1, 2, 2)
+BNE_RELATIVE = RelativeInstruction("zero",     0, 2, 2)
+BPL_RELATIVE = RelativeInstruction("negative", 0, 2, 2)
+
+def BRK(data, mos):
+    pass
+BRK_IMPLIED = ImpliedInstruction(BRK, 7, 1)
+
+BVC_RELATIVE = RelativeInstruction("overflow", 0, 2, 2)
+BVS_RELATIVE = RelativeInstruction("overflow", 1, 2, 2)
+
+def CLC(mos):
+    mos["carry"] = 0
+CLC_IMPLIED = ImpliedInstruction(CLC, 2, 1)
+
+def CLD(mos):
+    mos["decimal_mode"] = 0
+CLD_IMPLIED = ImpliedInstruction(CLD, 2, 1)
+
+def CLI(mos):
+    mos["irq_disable"] = 0
+CLI_IMPLIED = ImpliedInstruction(CLI, 2, 1)
+
+def CLV(mos):
+    mos["overflow"] = 0
+CLV_IMPLIED = ImpliedInstruction(CLV, 2, 1)
